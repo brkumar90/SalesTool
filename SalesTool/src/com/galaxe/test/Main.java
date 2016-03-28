666a9c22e7f71052c169cd387f2766b5923401eb
Brajesh Kumar <brkumar@galaxe.com>
2016-03-28 14:26:23 +0530

    commit

--- SalesTool/src/com/galaxe/test/Main.java
+++ SalesTool/src/com/galaxe/test/Main.java
@@ -8,12 +8,18 @@ public class Main {
 		
 		displayGreeting();
 		data.display();
+		
+		
 	}
 	
 	public static void displayGreeting(){
 		System.out.println("Hello Brajesh");
 		System.out.println("You are working in Galaxe");
 		System.out.println("I have completed 1 month ");
+		
+		
+		
+		
 		
 		
 		
