71c56d3e4b1da11b97f930572328e59bbb5cb120
Brajesh Kumar <brkumar@galaxe.com>
2016-03-28 13:35:14 +0530

    intial checkin of sales tool

---
+++ SalesTool/src/com/galaxe/test/Main.java
@@ -0,0 +1,19 @@
+package com.galaxe.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalaesData data = new SalaesData();
+		
+		
+		displayGreeting();
+		data.display();
+	}
+	
+	public static void displayGreeting(){
+		System.out.println("Hello Brajesh");
+		System.out.println("You are working in Galaxe");
+	}
+}
+
+
---
+++ SalesTool/src/com/galaxe/test/SalaesData.java
@@ -0,0 +1,10 @@
+package com.galaxe.test;
+
+public class SalaesData {
+
+	public void display() {
+	System.out.println("TODO Auto-generated method stub");
+		
+	}
+
+}
